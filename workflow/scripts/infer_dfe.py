996d04917a501a3a6c5aef539cbba5440ff8d728
Sendrowski <j.sendrowski18@gmail.com>
2025-04-28 09:47:53 +0900

    Update figures.

--- workflow/scripts/infer_dfe.py
+++ workflow/scripts/infer_dfe.py
@@ -23,6 +23,9 @@ inf.plot_discretized(show=False)
 plt.savefig('dfe_discretized.png')
 
 inf.plot_sfs_comparison(show=False)
+plt.xlabel('minor allele frequency')
+plt.ylabel('number of variants')
+plt.tight_layout()
 plt.savefig('sfs_comparison.png')
 
 pass
